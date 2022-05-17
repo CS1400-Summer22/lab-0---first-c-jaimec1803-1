0c8696421e7ae95a384af41a026cc746bf578583
jaimec1803 <105631179+jaimec1803@users.noreply.github.com>
2022-05-16 20:42:55 -0600

    Add files via upload
    First Coommit

---
+++ lab-0---first-c-jaimec1803/Program.cs
@@ -0,0 +1,28 @@
+﻿// NewMethod();
+// static void NewMethod()
+// {
+//     string sayHello = "Hello World!";
+// Console.WriteLine(sayHello);
+// sayHello = sayHello.Replace("Hello", "Greetings");
+// Console.WriteLine(sayHello);
+
+// string aFriend = "Jason";
+// Console.WriteLine("Hello " + aFriend);
+// aFriend = "Jeniffer";
+// Console.WriteLine("Hello " + aFriend);
+
+// string firstFriend = "Jason";
+// string secondFriend = "Jennifer";
+// Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+// Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+// Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+// string songLyrics = "You say goodbye, and I say hello";
+// Console.WriteLine(songLyrics.StartsWith("You"));
+// Console.WriteLine(songLyrics.StartsWith("goodbye"));
+
+// Console.WriteLine(songLyrics.EndsWith("hello"));
+// Console.WriteLine(songLyrics.EndsWith("goodbye"));
+
+// }
